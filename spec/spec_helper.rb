20b2937e4eb25cd1d6e01c0017af3a93b28bbf1d
Goh Matsumoto <urimaro@gmail.com>
2024-04-11 15:05:49 +0900

    Require ruby files in `lib/` and its subdirectories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+root_dir = File.expand_path(File.dirname(__FILE__) + "/..")
+Dir[File.join(root_dir, "/lib/**/*.rb")].each { |f| require f }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
