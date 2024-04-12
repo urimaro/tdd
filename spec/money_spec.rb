4e7384152f7832f934dd13dc18de60282dde3742
Goh Matsumoto <urimaro@gmail.com>
2024-04-12 14:22:16 +0900

    Add a spec: P14

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,7 +1,9 @@
 RSpec.describe "Money" do
   it "can be multiplied" do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq 10
+    product = five.times(2)
+    expect(product.amount).to eq 10
+    product = five.times(3)
+    expect(product.amount).to eq 15
   end
 end
