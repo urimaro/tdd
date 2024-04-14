1473fe32c64c53920b77fc11af070d843d8e58ef
Goh Matsumoto <urimaro@gmail.com>
2024-04-14 17:06:41 +0900

    Add Franc by coping Dollar: P31

---
+++ lib/franc.rb
@@ -0,0 +1,15 @@
+class Franc
+  attr_reader :amount
+
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def times(multiplier)
+    Franc.new(@amount * multiplier)
+  end
+
+  def ==(object)
+    @amount == object.amount
+  end
+end
