1850fab74a039d0ae2117c20d7985adc4354de97
Goh Matsumoto <urimaro@gmail.com>
2024-04-14 12:29:53 +0900

    Refactor specs to clarify intent: P26

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -2,9 +2,9 @@ RSpec.describe "Money" do
   it "can be multiplied" do
     five = Dollar.new(5)
     product = five.times(2)
-    expect(product.amount).to eq 10
+    expect(product).to eq Dollar.new(10)
     product = five.times(3)
-    expect(product.amount).to eq 15
+    expect(product).to eq Dollar.new(15)
   end
 
   it "is considered equal to another one with the same amount" do
