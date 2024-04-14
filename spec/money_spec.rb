0cc039cb8eb12a2b25e978b2b55d74f51e52a65e
Goh Matsumoto <urimaro@gmail.com>
2024-04-14 12:54:35 +0900

    Refactoring: Inline Variable: P26

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,10 +1,8 @@
 RSpec.describe "Money" do
   it "can be multiplied" do
     five = Dollar.new(5)
-    product = five.times(2)
-    expect(product).to eq Dollar.new(10)
-    product = five.times(3)
-    expect(product).to eq Dollar.new(15)
+    expect(five.times(2)).to eq Dollar.new(10)
+    expect(five.times(3)).to eq Dollar.new(15)
   end
 
   it "is considered equal to another one with the same amount" do
