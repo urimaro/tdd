ca85505769c3c3808f1d2ff4fbce62e719bea511
Goh Matsumoto <urimaro@gmail.com>
2024-04-17 10:56:56 +0900

    Modify other specs to use Money's Factory Method: P51

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,16 +1,16 @@
 RSpec.describe "Money" do
   it "can be multiplied" do
     five = Money.dollar(5)
-    expect(five.times(2)).to eq Dollar.new(10)
-    expect(five.times(3)).to eq Dollar.new(15)
+    expect(five.times(2)).to eq Money.dollar(10)
+    expect(five.times(3)).to eq Money.dollar(15)
   end
 
   it "is considered equal to another one with the same amount" do
-    expect(Dollar.new(5)).to eq Dollar.new(5)
-    expect(Dollar.new(5)).not_to eq Dollar.new(6)
+    expect(Money.dollar(5)).to eq Money.dollar(5)
+    expect(Money.dollar(5)).not_to eq Money.dollar(6)
     expect(Franc.new(5)).to eq Franc.new(5)
     expect(Franc.new(5)).not_to eq Franc.new(6)
-    expect(Franc.new(5)).not_to eq Dollar.new(5)
+    expect(Franc.new(5)).not_to eq Money.dollar(5)
   end
 
   it "can be multiplied with CHF" do
