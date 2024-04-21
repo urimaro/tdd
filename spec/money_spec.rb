7a9d93bdffdbc652dfa8790ad4a3dfbaf5fe8cd6
Goh Matsumoto <urimaro@gmail.com>
2024-04-21 15:01:09 +0900

    Remove Franc class: P78

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -8,23 +8,11 @@ RSpec.describe "Money" do
   it "is considered equal to another one with the same amount" do
     expect(Money.dollar(5)).to eq Money.dollar(5)
     expect(Money.dollar(5)).not_to eq Money.dollar(6)
-    expect(Money.franc(5)).to eq Money.franc(5)
-    expect(Money.franc(5)).not_to eq Money.franc(6)
     expect(Money.franc(5)).not_to eq Money.dollar(5)
-  end
-
-  it "can be multiplied with CHF" do
-    five = Money.franc(5)
-    expect(five.times(2)).to eq Money.franc(10)
-    expect(five.times(3)).to eq Money.franc(15)
   end
 
   it "returns its currency" do
     expect(Money.dollar(1).currency).to eq "USD"
     expect(Money.franc(1).currency).to eq "CHF"
-  end
-
-  it "is considered equal to objects of other classes" do
-    expect(Money.new(10, "CHF")).to eq Franc.new(10, "CHF")
   end
 end
