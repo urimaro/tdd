0238291f5a075efbdf5fb0045df8251fa478b8b9
Goh Matsumoto <urimaro@gmail.com>
2024-05-01 11:20:24 +0900

    Fix 'NotImplementedError: NotImplementedError': P124

--- lib/sum.rb
+++ lib/sum.rb
@@ -10,6 +10,10 @@ class Sum
     @addend = addend
   end
 
+  def plus(addend)
+    Sum.new(self, addend)
+  end
+
   def reduce(bank, to)
     amount = @augend.reduce(bank, to).amount + @addend.reduce(bank, to).amount
     Money.new(amount, to)
