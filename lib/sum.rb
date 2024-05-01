9174acc2a88832b3b76132d3c6e951807e31b6ea
Goh Matsumoto <urimaro@gmail.com>
2024-05-01 11:39:45 +0900

    Fix 'NotImplementedError: NotImplementedError': P126

--- lib/sum.rb
+++ lib/sum.rb
@@ -10,6 +10,10 @@ class Sum
     @addend = addend
   end
 
+  def times(multiplier)
+    Sum.new(@augend.times(multiplier), @addend.times(multiplier))
+  end
+
   def plus(addend)
     Sum.new(self, addend)
   end
