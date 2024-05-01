247b611641b46fb459ec5a4c22697a1fd193fe2b
Goh Matsumoto <urimaro@gmail.com>
2024-05-01 11:16:49 +0900

    Add one spec for Sum's `plus`: P123

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -63,4 +63,14 @@ RSpec.describe "Money" do
     result = bank.reduce(five_bucks.plus(ten_francs), "USD")
     expect(result).to eq Money.dollar(10)
   end
+
+  it "sum plus money" do
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate("CHF", "USD", 2)
+    sum = Sum.new(five_bucks, ten_francs).plus(five_bucks)
+    result = bank.reduce(sum, "USD")
+    expect(result).to eq Money.dollar(15)
+  end
 end
